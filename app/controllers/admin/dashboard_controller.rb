ce7534e3a9f1bfc88c8eec29e1d718df6eb80283
Aldope <aldompe95@gmail.com>
2023-03-16 14:03:48 -0600

    Remove first admin prototype

--- app/controllers/admin/dashboard_controller.rb
+++
@@ -1,7 +0,0 @@
-# app/controllers/admin/dashboard_controller.rb
-class Admin::DashboardController < ApplicationController
-  before_action :authenticate_user!
-
-  def index
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,6 @@ Rails.application.routes.draw do
   devise_for :users, only: :omniauth_callbacks, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   delete 'sign_out', to: 'sessions#destroy', as: :destroy_user_session
 
-  namespace :admin do
-    get 'dashboard', to: 'dashboard#index'
-  end
-
   resources :students do
     resources :nfts, only: [:new, :create, :destroy]
   end
